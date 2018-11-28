3c864f16ae4cc2cffcb60bf68c034e3bb857ad56
Dustin Spicuzza <dustin@virtualroadside.com>
2018-11-28 00:15:11 -0500

    Format python code using black

--- data_analyzer.py
+++ data_analyzer.py
@@ -40,34 +40,30 @@ MOTION_THRESHOLD = 0.1
 # You probably don't have to change anything else
 #
 
-TIME_COL = columns['time']
-BATTERY_COL = columns['battery']
-AUTOSPEED_COL = columns['autospeed']
-L_VOLTS_COL = columns['l_volts']
-R_VOLTS_COL = columns['r_volts']
-L_ENCODER_P_COL = columns['l_encoder_pos']
-R_ENCODER_P_COL = columns['r_encoder_pos']
-L_ENCODER_V_COL = columns['l_encoder_vel']
-R_ENCODER_V_COL = columns['r_encoder_vel']
+TIME_COL = columns["time"]
+BATTERY_COL = columns["battery"]
+AUTOSPEED_COL = columns["autospeed"]
+L_VOLTS_COL = columns["l_volts"]
+R_VOLTS_COL = columns["r_volts"]
+L_ENCODER_P_COL = columns["l_encoder_pos"]
+R_ENCODER_P_COL = columns["r_encoder_pos"]
+L_ENCODER_V_COL = columns["l_encoder_vel"]
+R_ENCODER_V_COL = columns["r_encoder_vel"]
 
-JSON_DATA_KEYS = [
-    'slow-forward',
-    'slow-backward',
-    'fast-forward',
-    'fast-backward'
-]
+JSON_DATA_KEYS = ["slow-forward", "slow-backward", "fast-forward", "fast-backward"]
 
 # From 449's R script
 def smoothDerivative(tm, value, n, off):
-    '''
+    """
         :param tm: time column
         :param value: Value to take the derivative of
         :param off: Offset of time column start position
-    '''
+    """
     dlen = len(value)
-    dt = tm[n+1+off:dlen+off] - tm[1+off:(dlen-n)+off]
-    x = (value[(n+1):dlen] - value[1:(dlen-n)]) / dt
+    dt = tm[n + 1 + off : dlen + off] - tm[1 + off : (dlen - n) + off]
+    x = (value[(n + 1) : dlen] - value[1 : (dlen - n)]) / dt
     return x
+
 
 # trim functions: expect array of tm, volts, velocity, acceleration
 TRIM_TM_COL = 0
@@ -78,41 +74,63 @@ TRIM_ACC_COL = 4
 
 TRIM_MAX_COL = TRIM_ACC_COL
 
+
 def trim_quasi_testdata(data):
     # removes initial minimal velocity points
-    for i,v  in enumerate(data[TRIM_VEL_COL]):
+    for i, v in enumerate(data[TRIM_VEL_COL]):
         if abs(v) > 0.1:
             break
-    return data[:,i:]
-    
+    return data[:, i:]
+
+
 def trim_step_testdata(data):
     # removes anything before the max acceleration
     max_accel_idx = np.argmax(np.abs(data[TRIM_ACC_COL]))
-    return data[:,max_accel_idx:]
+    return data[:, max_accel_idx:]
+
 
 def prepare_data(data, trimfn, scale, window):
-    
+
     # deal with incomplete data
-    if len(data) < window*2:
-        return np.zeros(shape=(TRIM_MAX_COL+1, 4)), np.zeros(shape=(TRIM_MAX_COL+1, 4))
-    
+    if len(data) < window * 2:
+        return (
+            np.zeros(shape=(TRIM_MAX_COL + 1, 4)),
+            np.zeros(shape=(TRIM_MAX_COL + 1, 4)),
+        )
+
     # Transform the data into a numpy array to make it easier to use
     data = np.array(data).transpose()
-    
+
     data[L_ENCODER_P_COL] *= scale
     data[R_ENCODER_P_COL] *= scale
     data[L_ENCODER_V_COL] *= scale
     data[R_ENCODER_V_COL] *= scale
-    
+
     l_acc = smoothDerivative(data[TIME_COL], data[L_ENCODER_V_COL], window, 0)
     r_acc = smoothDerivative(data[TIME_COL], data[R_ENCODER_V_COL], window, 0)
-    
+
     # trim data to ensure it's all the same length to ease analysis
-    data = data[:,1:-window]
-    
-    l = np.vstack((data[TIME_COL], data[L_VOLTS_COL], data[L_ENCODER_P_COL], data[L_ENCODER_V_COL], l_acc))
-    r = np.vstack((data[TIME_COL], data[R_VOLTS_COL], data[R_ENCODER_P_COL], data[R_ENCODER_V_COL], r_acc))
-    
+    data = data[:, 1:-window]
+
+    l = np.vstack(
+        (
+            data[TIME_COL],
+            data[L_VOLTS_COL],
+            data[L_ENCODER_P_COL],
+            data[L_ENCODER_V_COL],
+            l_acc,
+        )
+    )
+    r = np.vstack(
+        (
+            data[TIME_COL],
+            data[R_VOLTS_COL],
+            data[R_ENCODER_P_COL],
+            data[R_ENCODER_V_COL],
+            r_acc,
+        )
+    )
+
     l = trimfn(l)
     r = trimfn(r)
 
@@ -120,7 +138,7 @@ def prepare_data(data, trimfn, scale, window):
 
 
 def analyze_data(data, scale=1.0, window=WINDOW):
-    '''
+    """
         Firstly, data should be "trimmed" to exclude any data points at which the
         robot was not being commanded to do anything. [we don't have to do this]
         
@@ -148,98 +166,111 @@ def analyze_data(data, scale=1.0, window=WINDOW):
         Each data pool will then yield three parameters -
         intercept, Kv (the regression coefficient of velocity), and Ka (the regression
         coefficient of acceleration).
-    '''
-    
-    sf_l, sf_r = prepare_data(data['slow-forward'], trim_quasi_testdata, scale, window)
-    sb_l, sb_r = prepare_data(data['slow-backward'], trim_quasi_testdata, scale, window)
+    """
 
-    ff_l, ff_r = prepare_data(data['fast-forward'], trim_step_testdata, scale, window)
-    fb_l, fb_r = prepare_data(data['fast-backward'], trim_step_testdata, scale, window)
-    
+    sf_l, sf_r = prepare_data(data["slow-forward"], trim_quasi_testdata, scale, window)
+    sb_l, sb_r = prepare_data(data["slow-backward"], trim_quasi_testdata, scale, window)
+
+    ff_l, ff_r = prepare_data(data["fast-forward"], trim_step_testdata, scale, window)
+    fb_l, fb_r = prepare_data(data["fast-backward"], trim_step_testdata, scale, window)
+
     # Now that we have useful data, perform linear regression on it
     def _ols(x1, x2, y):
-        '''multivariate linear regression using ordinary least squares'''
+        """multivariate linear regression using ordinary least squares"""
         ox = np.array((x1, x2)).T
         x = np.c_[np.ones(ox.shape[0]), ox]
         return np.linalg.lstsq(x, y, rcond=None)[0]
-    
+
     def _print(n, pfx, qu, step):
         vel = np.concatenate((qu[TRIM_VEL_COL], step[TRIM_VEL_COL]))
         accel = np.concatenate((qu[TRIM_ACC_COL], step[TRIM_ACC_COL]))
         volts = np.concatenate((qu[TRIM_V_COL], step[TRIM_V_COL]))
-        
+
         vi, kv, ka = _ols(vel, accel, volts)
-        
+
         txt = "%s:  kv=%.4f ka=%.4f vintercept=%.4f" % (pfx, kv, ka, vi)
         print(txt)
-        
+
         plt.figure(txt)
-        
+
         ax = plt.subplot(211)
-        ax.set_xlabel('voltage')
-        ax.set_ylabel('velocity')
-        plt.scatter(volts, vel, marker='.', c='#000000')
-        
+        ax.set_xlabel("voltage")
+        ax.set_ylabel("velocity")
+        plt.scatter(volts, vel, marker=".", c="#000000")
+
         # show the fit
         y = np.linspace(np.min(vel), np.max(vel))
-        plt.plot(kv*y + vi, y)
-        
+        plt.plot(kv * y + vi, y)
+
         ax = plt.subplot(212)
-        ax.set_xlabel('voltage')
-        ax.set_ylabel('acceleration')
-        plt.scatter(volts, accel, marker='.', c='#000000')
-        
+        ax.set_xlabel("voltage")
+        ax.set_ylabel("acceleration")
+        plt.scatter(volts, accel, marker=".", c="#000000")
+
         y = np.linspace(np.min(accel), np.max(accel))
-        plt.plot(ka*y, y)
-    
+        plt.plot(ka * y, y)
+
     # kv and vintercept is computed from the first two tests, ka from the latter
-    _print(1, 'Left forward  ', sf_l, ff_l)
-    _print(2, 'Left backward ', sb_l, fb_l)
-    
-    _print(3, 'Right forward ', sf_r, ff_r)
-    _print(4, 'Right backward', sb_r, fb_r)
-    
+    _print(1, "Left forward  ", sf_l, ff_l)
+    _print(2, "Left backward ", sb_l, fb_l)
+
+    _print(3, "Right forward ", sf_r, ff_r)
+    _print(4, "Right backward", sb_r, fb_r)
+
     plt.show()
 
+
 def split_to_csv(fname, stored_data):
-    
+
     outdir = dirname(fname)
     fname = join(outdir, splitext(basename(fname))[0])
-    
-    header = ['']*(max(columns.values())+1)
+
+    header = [""] * (max(columns.values()) + 1)
     for k, v in columns.items():
         header[v] = k
-        
+
     for d in JSON_DATA_KEYS:
-        fn = '%s-%s.csv' % (fname, d)
+        fn = "%s-%s.csv" % (fname, d)
         if exists(fn):
             print("Error:", fn, "already exists")
             return
-    
+
     for d in JSON_DATA_KEYS:
-        fn = '%s-%s.csv' % (fname, d)
-        with open(fn, 'w') as fp:
+        fn = "%s-%s.csv" % (fname, d)
+        with open(fn, "w") as fp:
             c = csv.writer(fp)
             c.writerow(header)
             for r in stored_data[d]:
                 c.writerow(r)
 
+
 def main():
-    
+
     parser = argparse.ArgumentParser(description="Analyze your data")
-    parser.add_argument('jsonfile', help="Input JSON file")
-    parser.add_argument('--to-csv', action='store_true', default=False)
-    parser.add_argument('--scale', default=1, type=float, help="Multiply position/velocity values by this")
-    parser.add_argument('--window', default=WINDOW, type=int, help="Window size for computing acceleration")
+    parser.add_argument("jsonfile", help="Input JSON file")
+    parser.add_argument("--to-csv", action="store_true", default=False)
+    parser.add_argument(
+        "--scale",
+        default=1,
+        type=float,
+        help="Multiply position/velocity values by this",
+    )
+    parser.add_argument(
+        "--window",
+        default=WINDOW,
+        type=int,
+        help="Window size for computing acceleration",
+    )
     args = parser.parse_args()
-    
-    with open(args.jsonfile, 'r') as fp:
+
+    with open(args.jsonfile, "r") as fp:
         stored_data = json.load(fp)
-    
+
     if args.to_csv:
         split_to_csv(args.jsonfile, stored_data)
     else:
         analyze_data(stored_data, scale=args.scale, window=args.window)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- robot-python-talonsrx/physics.py
+++ robot-python-talonsrx/physics.py
@@ -9,108 +9,118 @@ from pyfrc.physics.units import units
 
 
 class PhysicsEngine(object):
-    '''
+    """
        Simulates a 4-wheel robot using Tank Drive joystick control
-    '''
-    
-    WHEEL_DIAMETER = 6*units.inch
+    """
+
+    WHEEL_DIAMETER = 6 * units.inch
     ENCODER_PULSE_PER_REV = 4096
-    
-    ENCODER_SCALE = (units.foot.m_from(WHEEL_DIAMETER) * math.pi / ENCODER_PULSE_PER_REV)
-    
+
+    ENCODER_SCALE = units.foot.m_from(WHEEL_DIAMETER) * math.pi / ENCODER_PULSE_PER_REV
+
     def __init__(self, physics_controller):
-        '''
+        """
             :param physics_controller: `pyfrc.physics.core.Physics` object
                                        to communicate simulation effects to
-        '''
-        
+        """
+
         self.physics_controller = physics_controller
-        
+
         #
         # Two ways of initializing the realistic physics -- either use the
         # ka/kv that you computed for your robot, or use the theoretical model
         #
-        
+
         # Change these parameters to fit your robot!
         # -> these parameters are for the test robot mentioned in the paper
         motor_cfg = motor_cfgs.MOTOR_CFG_CIM
-        robot_mass = 110*units.lbs
-        
-        bumper_width = 3.25*units.inch
-        robot_wheelbase = 22*units.inch
-        robot_width = 23*units.inch + bumper_width*2
-        robot_length = 32*units.inch + bumper_width*2
-        
-        wheel_diameter = 3.8*units.inch
+        robot_mass = 110 * units.lbs
+
+        bumper_width = 3.25 * units.inch
+        robot_wheelbase = 22 * units.inch
+        robot_width = 23 * units.inch + bumper_width * 2
+        robot_length = 32 * units.inch + bumper_width * 2
+
+        wheel_diameter = 3.8 * units.inch
         drivetrain_gear_ratio = 6.1
         motors_per_side = 3
-        
+
         # Uses theoretical parameters by default, change this if you've
         # actually measured kv/ka for your robot
         if True:
-        
-            self.drivetrain = tankmodel.TankModel.theory(motor_cfg,
-                                                         robot_mass, drivetrain_gear_ratio, motors_per_side,
-                                                         robot_wheelbase,
-                                                         robot_width,
-                                                         robot_length,
-                                                         wheel_diameter)
+
+            self.drivetrain = tankmodel.TankModel.theory(
+                motor_cfg,
+                robot_mass,
+                drivetrain_gear_ratio,
+                motors_per_side,
+                robot_wheelbase,
+                robot_width,
+                robot_length,
+                wheel_diameter,
+            )
         else:
             # These are the parameters for kv/ka that you computed for your robot
             # -> this example uses the values from the paper
-            l_kv = 0.81*units.tm_kv
-            l_ka = 0.21*units.tm_ka
-            l_vintercept = 1.26*units.volts
-            r_kv = 0.81*units.tm_kv
-            r_ka = 0.21*units.tm_ka
-            r_vintercept = 1.26*units.volts
-            
-            self.drivetrain = tankmodel.TankModel(motor_cfg,
-                                                  robot_mass,
-                                                  robot_wheelbase,
-                                                  robot_width,
-                                                  robot_length,
-                                                  l_kv, l_ka, l_vintercept,
-                                                  r_kv, r_ka, r_vintercept)
-            
+            l_kv = 0.81 * units.tm_kv
+            l_ka = 0.21 * units.tm_ka
+            l_vintercept = 1.26 * units.volts
+            r_kv = 0.81 * units.tm_kv
+            r_ka = 0.21 * units.tm_ka
+            r_vintercept = 1.26 * units.volts
+
+            self.drivetrain = tankmodel.TankModel(
+                motor_cfg,
+                robot_mass,
+                robot_wheelbase,
+                robot_width,
+                robot_length,
+                l_kv,
+                l_ka,
+                l_vintercept,
+                r_kv,
+                r_ka,
+                r_vintercept,
+            )
 
     def update_sim(self, hal_data, now, tm_diff):
-        '''
+        """
             Called when the simulation parameters for the program need to be
             updated.
             
             :param now: The current time as a float
             :param tm_diff: The amount of time that has passed since the last
                             time that this function was called
-        '''
-        
+        """
+
         # TODO: simulate battery voltage effects
-        hal_data['power']['vin_voltage'] = 12.0
-        
+        hal_data["power"]["vin_voltage"] = 12.0
+
         # Simulate the drivetrain
         try:
-            lf_motor = hal_data['CAN'][1]
-            rf_motor = hal_data['CAN'][2]
+            lf_motor = hal_data["CAN"][1]
+            rf_motor = hal_data["CAN"][2]
         except (KeyError, IndexError):
             return
-        
+
         # Not needed because front and rear should be in sync
-        #lr_motor = hal_data['pwm'][3]['value']
-        #rr_motor = hal_data['pwm'][4]['value']
-        
-        x, y, angle = self.drivetrain.get_distance(lf_motor['value'] * -1, rf_motor['value'] * -1, tm_diff)
+        # lr_motor = hal_data['pwm'][3]['value']
+        # rr_motor = hal_data['pwm'][4]['value']
+
+        x, y, angle = self.drivetrain.get_distance(
+            lf_motor["value"] * -1, rf_motor["value"] * -1, tm_diff
+        )
         self.physics_controller.distance_drive(x, y, angle)
-        
+
         # set the position / velocity on the encoder
-        
+
         left_counter = self.drivetrain.l_position / self.ENCODER_SCALE
         right_counter = self.drivetrain.r_position / self.ENCODER_SCALE
-        lf_motor['quad_position'] = int(left_counter)
-        rf_motor['quad_position'] = int(right_counter)
-        
+        lf_motor["quad_position"] = int(left_counter)
+        rf_motor["quad_position"] = int(right_counter)
+
         # The 10 is because velocity is measured per 100ms
         left_velocity = 10 * self.drivetrain.l_velocity / self.ENCODER_SCALE
         right_velocity = 10 * self.drivetrain.r_velocity / self.ENCODER_SCALE
-        lf_motor['quad_velocity'] = int(left_velocity)
-        rf_motor['quad_velocity'] = int(right_velocity)
-        
+        lf_motor["quad_velocity"] = int(left_velocity)
+        rf_motor["quad_velocity"] = int(right_velocity)
