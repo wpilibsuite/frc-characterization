8fb7d0c47cb08a706c8a9f08382112c40ce1d46e
Eli <emichaelbarnett@gmail.com>
2019-09-29 15:26:47 -0400

    increment version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="robotpy-characterization",
-    version="0.0.5",
+    version="0.0.6",
     author="Eli Barnett, Dustin Spicuzza",
     author_email="emichaelbarnett@gmail.com, dustin@virtualroadside.com",
     description="RobotPy Characterization Library",
