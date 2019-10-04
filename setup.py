55d5e9d7091e52eeb7442028a36ea47200664383
Eli <emichaelbarnett@gmail.com>
2019-10-04 16:53:42 -0400

    holyshitthat'sareadme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="robotpy-characterization",
-    version="0.1.0",
+    version="0.1.1",
     author="Eli Barnett, Dustin Spicuzza",
     author_email="emichaelbarnett@gmail.com, dustin@virtualroadside.com",
     description="RobotPy Characterization Library",
