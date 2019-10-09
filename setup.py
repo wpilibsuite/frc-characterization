a8ee8dc55f26720bafb8a973262db3cf491931a8
Eli <emichaelbarnett@gmail.com>
2019-10-09 18:04:33 -0400

    fix small error in drive talon proj

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='robotpy-characterization',
-    version='0.1.3',
+    version='0.1.5',
     author='Eli Barnett, Dustin Spicuzza',
     author_email='emichaelbarnett@gmail.com, dustin@virtualroadside.com',
     description='RobotPy Characterization Library',
