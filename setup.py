d5504727103f5dbb018ad76439b046f3fc6a69cf
Eli <emichaelbarnett@gmail.com>
2019-10-09 18:06:25 -0400

    add readme to pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 import setuptools
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setuptools.setup(
     name='robotpy-characterization',
-    version='0.1.5',
+    version='0.1.6',
     author='Eli Barnett, Dustin Spicuzza',
     author_email='emichaelbarnett@gmail.com, dustin@virtualroadside.com',
     description='RobotPy Characterization Library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=[
         'arm_characterization',
         'drive_characterization',
