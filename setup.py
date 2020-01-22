6528b9ef19f92b20de912d30b5e05f03135fb48b
oblarg <emichaelbarnett@gmail.com>
2020-01-22 15:39:11 -0500

    update readme to point to frc-docs

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ else:
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "README_pypi.md"), encoding="utf-8") as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setuptools.setup(
